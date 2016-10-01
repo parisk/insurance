67d6cf14be4b89a26eab2dd29f4133189c2a6e89
Paris Kasidiaris <paris@sourcelair.com>
2016-10-01 12:31:52 +0300

    Added plans

--- companies/admin.py
+++ companies/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-
 from companies.models import Company
+from companies.models import Plan
+from companies.models import Sector
+
 
 class CompanyAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Company, CompanyAdmin)
+
+
+class SectorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Sector, SectorAdmin)
+
+
+class PlanAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Plan, PlanAdmin)
--- companies/models.py
+++ companies/models.py
@@ -5,3 +5,31 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
+class Sector(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
+class Plan(models.Model):
+    name = models.CharField(max_length=255)
+    company = models.ForeignKey(Company)
+    sector = models.ForeignKey(Sector)
+
+    def __unicode__(self):
+        return '%s from %s for %s' % (self.name, self.company, self.sector)
+
+    def __str__(self):
+        return '%s from %s for %s' % (self.name, self.company, self.sector)
